a5a6cfb94beaa65a94f7ccb1a25ee18acb216e13
kaztro <00229017@uca.edu.sv>
2018-05-23 19:58:56 -0600

    Al fin termine con TODO we

--- LABO_00229017/src/gui/Ventana.java
+++ LABO_00229017/src/gui/Ventana.java
@@ -17,7 +17,7 @@ import java.util.regex.Pattern;
  * @author LN710Q
  */
 public class Ventana extends JFrame{
-    private int WIDTH = 500, HEIGHT = 300, withTF = 240, heightTF = 40, widthBT = 60, heightBT = 30;
+    private int WIDTH = 600, HEIGHT = 270, withTF = 150, heightTF = 30, widthBT = 50, heightBT = 30;
     JButton suma, resta, multiplicacion, division, binario;
     JTextField val1, val2, result, cajaC, resultC;
 
@@ -33,43 +33,43 @@ public class Ventana extends JFrame{
         Container container = getContentPane();
 
         suma = new JButton("+");
-        suma.setBounds(200, 115, widthBT, heightBT);
+        suma.setBounds(10,80, widthBT, heightBT);
         container.add(suma);
 
         resta = new JButton("-");
-        resta.setBounds(200, 115, widthBT, heightBT);
+        resta.setBounds(60,80, widthBT, heightBT);
         container.add(resta);
 
         multiplicacion = new JButton("*");
-        multiplicacion.setBounds(200, 115, widthBT, heightBT);
+        multiplicacion.setBounds(110,80, widthBT, heightBT);
         container.add(multiplicacion);
 
         division = new JButton("/");
-        division.setBounds(200, 115, widthBT, heightBT);
+        division.setBounds(160,80, widthBT, heightBT);
         container.add(division);
 
         binario = new JButton("Binary");
-        binario.setBounds(200, 115, widthBT, heightBT);
+        binario.setBounds(210,170, widthBT+50, heightBT);
         container.add(binario);
 
-        val1 = new JTextField("Valor 1");
-        val1.setBounds(145, 40, withTF, heightTF);
+        val1 = new JTextField();
+        val1.setBounds(10, 30, withTF, heightTF);
         container.add(val1);
 
-        val2 = new JTextField("Valor 2");
-        val2.setBounds(145, 60, withTF, heightTF);
+        val2 = new JTextField();
+        val2.setBounds(210, 30, withTF, heightTF);
         container.add(val2);
 
-        result = new JTextField("Resultado");
-        result.setBounds(145, 80, withTF, heightTF);
+        result = new JTextField();
+        result.setBounds(10,170, withTF, heightTF);
         container.add(result);
 
-        cajaC = new JTextField("A Binario");
-        cajaC.setBounds(145, 80, withTF, heightTF);
+        cajaC = new JTextField();
+        cajaC.setBounds(400,30, withTF, heightTF);
         container.add(cajaC);
 
-        resultC = new JTextField("A Binario");
-        resultC.setBounds(145, 80, withTF, heightTF);
+        resultC = new JTextField();
+        resultC.setBounds(400,170, withTF, heightTF);
         container.add(resultC);
 
 
@@ -93,7 +93,7 @@ public class Ventana extends JFrame{
                         break;
                     }
                     else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
+                        JOptionPane.showMessageDialog(null, "No es un numero, ingreselo nuevamente.");
                         break;
                     }
                 }                }
@@ -117,7 +117,7 @@ public class Ventana extends JFrame{
                         break;
                     }
                     else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
+                        JOptionPane.showMessageDialog(null, "No es un numero, ingreselo nuevamente.");
                         break;
                     }
                 }
@@ -142,7 +142,7 @@ public class Ventana extends JFrame{
                         break;
                     }
                     else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
+                        JOptionPane.showMessageDialog(null, "No es un numero, ingreselo nuevamente.");
                         break;
                     }
                 }
@@ -167,7 +167,7 @@ public class Ventana extends JFrame{
                         break;
                     }
                     else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
+                        JOptionPane.showMessageDialog(null, "No es un numero, ingreselo nuevamente.");
                         break;
                     }
                 }
@@ -178,7 +178,7 @@ public class Ventana extends JFrame{
             public void actionPerformed(ActionEvent e){
                 AbstractFactory factory;
                 factory =  FactoryProducer.getFactory("conversor");
-                Conversor conver = factory.getConversor("binario");
+                Conversor conver = factory.getConversor("Binario");
                 String cont3 = cajaC.getText();
 
                 Pattern patron = Pattern.compile("\\d*");
@@ -191,15 +191,14 @@ public class Ventana extends JFrame{
                         break;
                     }
                     else{
-                        System.out.println("No puede convertirse a binario porque no es un numero o no es positivo.");
-                        System.out.println("Ingreselo nuevamente");
+                        JOptionPane.showMessageDialog(null, "No es un numero o no es positivo, intenta denuevo");
                         break;
                     }
                 }
 
             }
         });
-        setSize(600,270);
+        setSize(WIDTH,HEIGHT);
     }
     public static void main(String[] args){
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -213,219 +212,3 @@ public class Ventana extends JFrame{
 
 
 
-/*
-  public class Ventana extends JFrame{
-    private JTextField caja1, caja2, caja3, caja4, caja5;
-    private JButton botonSu, botonRe, botonMu, botonDi, botonCo;
-    private JLabel label1, label2;
-
-
-
-    public Ventana(){
-        super("Titulo de la ventana");
-        initComponent();
-    }
-
-    public void initComponent(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
-
-        Container contenedor = getContentPane();
-
-        label1 = new JLabel("Calculadora");
-        label1.setBounds(140,4,500,20);
-        contenedor.add(label1);
-
-        caja1 = new JTextField("Numero1");
-        caja1.setBounds(10,30,150,30);
-        contenedor.add(caja1);
-
-        caja2 = new JTextField("Numero2");
-        caja2.setBounds(210,30,150,30);
-        contenedor.add(caja2);
-
-        botonSu = new JButton("+");
-        botonSu.setBounds(10,80,50,30);
-        contenedor.add(botonSu);
-
-        botonRe = new JButton("-");
-        botonRe.setBounds(60,80,50,30);
-        contenedor.add(botonRe);
-
-        botonMu = new JButton("*");
-        botonMu.setBounds(110,80,50,30);
-        contenedor.add(botonMu);
-
-        botonDi = new JButton("/");
-        botonDi.setBounds(160,80,50,30);
-        contenedor.add(botonDi);
-
-        label2 = new JLabel("Conversor");
-        label2.setBounds(140,130,500,20);
-        contenedor.add(label2);
-
-        caja3 = new JTextField("Numero3");
-        caja3.setBounds(10,170,150,30);
-        contenedor.add(caja3);
-
-        botonCo = new JButton("a binario");
-        botonCo.setBounds(210,170,150,30);
-        contenedor.add(botonCo);
-
-        caja4 = new JTextField();
-        caja4.setBounds(400,30,150,30);
-        contenedor.add(caja4);
-
-        caja5 = new JTextField("");
-        caja5.setBounds(400,170,150,30);
-        contenedor.add(caja5);
-
-        botonSu.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                AbstractFactory factory;
-                factory =  FactoryProducer.getFactory("aritmetica");
-                Aritmetica suma = factory.getAritmetica("suma");
-                String cont1 = caja1.getText();
-                String cont2 = caja2.getText();
-
-                Pattern patron = Pattern.compile("\\d*");
-                Matcher matcher = patron.matcher(cont1);
-                Matcher matcher2 = patron.matcher(cont2);
-
-                while(true){
-                    if(matcher.matches() && matcher2.matches()){
-                        float result = suma.operacion(Integer.parseInt(caja1.getText()),Integer.parseInt(caja2.getText()));
-                        caja4.setText(Float.toString(result));
-                        break;
-                    }
-                    else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
-                        break;
-                    }
-                }
-            }
-        });
-
-        botonRe.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                AbstractFactory factory;
-                factory =  FactoryProducer.getFactory("aritmetica");
-                Aritmetica resta = factory.getAritmetica("resta");
-                String cont1 = caja1.getText();
-                String cont2 = caja2.getText();
-
-                Pattern patron = Pattern.compile("\\d*");
-                Matcher matcher = patron.matcher(cont1);
-                Matcher matcher2 = patron.matcher(cont2);
-
-            while(true){
-                if(matcher.matches() && matcher2.matches()){
-                    float result = resta.operacion(Integer.parseInt(caja1.getText()),Integer.parseInt(caja2.getText()));
-                    caja4.setText(Float.toString(result));
-                    break;
-                }
-                else{
-                    System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
-                    break;
-                }
-            }
-            }
-        });
-
-        botonMu.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                AbstractFactory factory;
-                factory =  FactoryProducer.getFactory("aritmetica");
-                Aritmetica multi = factory.getAritmetica("multiplicacion");
-                String cont1 = caja1.getText();
-                String cont2 = caja2.getText();
-
-                Pattern patron = Pattern.compile("\\d*");
-                Matcher matcher = patron.matcher(cont1);
-                Matcher matcher2 = patron.matcher(cont2);
-
-                while(true){
-                    if(matcher.matches() && matcher2.matches()){
-                        float result = multi.operacion(Integer.parseInt(caja1.getText()),Integer.parseInt(caja2.getText()));
-                        caja4.setText(Float.toString(result));
-                        break;
-                    }
-                    else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
-                        break;
-                    }
-                }
-            }
-        });
-
-        botonDi.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                AbstractFactory factory;
-                factory =  FactoryProducer.getFactory("aritmetica");
-                Aritmetica div = factory.getAritmetica("division");
-                String cont1 = caja1.getText();
-                String cont2 = caja2.getText();
-
-                Pattern patron = Pattern.compile("\\d*");
-                Matcher matcher = patron.matcher(cont1);
-                Matcher matcher2 = patron.matcher(cont2);
-
-                while(true){
-                    if(matcher.matches() && matcher2.matches()){
-                        float result = div.operacion(Integer.parseInt(caja1.getText()),Integer.parseInt(caja2.getText()));
-                        caja4.setText(Float.toString(result));
-                        break;
-                    }
-                    else{
-                        System.out.println("No es un numero o no es entero, ingreselo nuevamente.");
-                        break;
-                    }
-                }
-
-            }
-        });
-
-        botonCo.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                AbstractFactory factory;
-                factory =  FactoryProducer.getFactory("conversor");
-                Conversor conver = factory.getConversor("binario");
-                String cont3 = caja3.getText();
-
-                Pattern patron = Pattern.compile("\\d*");
-                Matcher matcher = patron.matcher(cont3);
-
-                while(true){
-                    if(matcher.matches()){
-                        String result = conver.convertir(Integer.parseInt(caja3.getText()));
-                        caja5.setText(result);
-                        break;
-                    }
-                    else{
-                        System.out.println("No puede convertirse a binario porque no es un numero o no es positivo.");
-                        System.out.println("Ingreselo nuevamente");
-                        break;
-                    }
-                }
-
-            }
-        });
-
-        setSize(600,270);
-    }
-
-    public static void main(String[] args){
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run(){
-                new Ventana().setVisible(true);
-            }
-        });
-    }
-}
-*/
--- LABO_00229017/src/labo_00229017/Conversiones/Binario.java
+++ LABO_00229017/src/labo_00229017/Conversiones/Binario.java
@@ -24,18 +24,6 @@ public class Binario implements Conversor {
     }
 }
 
-        /*int exp, digito;
-        double binario;
-        exp=0;
-        binario=0;
-        while(a!=0){
-                digito = a % 2;           
-                binario = binario + digito * Math.pow(10, exp);  
-                exp++;
-                a = a/2;
-        }
-        return binario;
-    }*/
 
    
     
